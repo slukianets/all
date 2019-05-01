ca348586659cbd985a44c903276a022b98e68587
slukianets <lukyanets89@gmail.com>
2019-05-01 04:07:06 -0700

    add list of ip addresses

--- python-scapy-ping.py
+++ python-scapy-ping.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python
 from contextlib import contextmanager
-import sys, os 
+import sys, os, netaddr 
 from scapy.all import *
 
 @contextmanager
@@ -13,17 +13,24 @@ def outnull():
         finally:
             sys.stdout = old_stdout
 
-dest_ip =str(sys.argv[1])
-#print(dest_ip)
-package = IP(dst = dest_ip)/ICMP()
-with outnull():
-    reply,unasw = sr(package, timeout = 0.2)
+network = netaddr.IPNetwork(str(sys.argv[1]))
+list_ip_addr = []
+for ip_addr in network:
+    list_ip_addr.append(str(ip_addr))
+del(list_ip_addr[0])
+del(list_ip_addr[-1])
+#print(list_ip_addr)
+for dest_ip in list_ip_addr:
+    print(dest_ip)
+    package = IP(dst = dest_ip)/ICMP()
+    with outnull():
+        reply,unasw = sr(package, timeout = 0.1)
 #print(reply[0])
-if reply:
-    if((reply[0][1].ihl == 5) and (reply[0][1].src == dest_ip)):
-        print("Host " + dest_ip + " is online")
-else:
-     print("Host " + dest_ip + " is offline")
+    if reply:
+        if((reply[0][1].ihl == 5) and (reply[0][1].src == dest_ip)):
+            print("Host " + dest_ip + " is online")
+    else:
+       print("Host " + dest_ip + " is offline")
 
 
 
