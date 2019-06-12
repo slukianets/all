81ed71c1ed4d6af47dc508d6be9cb5efe5616066
root <root@localhost.localdomain>
2019-06-12 05:59:05 -0400

    fix

--- python-mac-changer.py
+++ python-mac-changer.py
@@ -17,7 +17,7 @@ def get_agruments():
     return options
 
 def change_mac(interface, new_mac):
-    print("[+] Changing MAC address for " + interface + "to" + new_mac)
+    print("[+] Changing MAC address for " + interface + " to " + new_mac)
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
@@ -35,7 +35,7 @@ current_mac = get_current_mac(options.interface)
 print("Current MAC is " + current_mac)
 change_mac(options.interface, options.new_mac)
 current_mac = get_current_mac(options.interface)
-if current_mac == options.interface:
+if current_mac == options.new_mac:
     print("[+] MAC address was successfully changed to " + current_mac)
 else:
     print("[-] Mac address did not het changed.")
