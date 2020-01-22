8aa3b81e16d004d7a91c5530839929ab32ef558d
lukianets <lukyanets89@gmail.com>
2020-01-22 17:50:01 +0200

    add arguments 2

--- python-scapy-arp-scan.py
+++ python-scapy-arp-scan.py
@@ -3,11 +3,15 @@
 import scapy.all as scapy
 import argparse
 
+
 def get_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--target", dest = "target", help = "Target IP address and IP range")
     options = parser.parse_args()
+    if not options:
+        parser.error("[-] Please specify an IP address or an IP range, use --help for more info.")
     return options
+
 
 def scan(ip):
     arp_request = scapy.ARP(pdst = ip)
@@ -18,13 +22,14 @@ def scan(ip):
         list_arp_response.append({"ip" : reply[1].psrc, "mac" : reply[1].hwsrc})
     return list_arp_response
 
+
 def output_results(list):
     print("IP\t\t\tMAC")
     print("-" * 42)
     for element in list:
         print(element["ip"],"\t\t", element["mac"])
         print("-" * 42)
-        
+
 options = get_arguments()
 scan_result = scan(options.target)
 output_results(scan_result)
