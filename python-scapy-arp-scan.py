515bc73f23d5c9bdf123d13022548c7d8f188223
lukianets <lukyanets89@gmail.com>
2020-01-22 16:55:00 +0200

    output

--- python-scapy-arp-scan.py
+++ python-scapy-arp-scan.py
@@ -7,7 +7,7 @@ def scan(ip):
     broadcast_frame = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")
     answered, unanswered = scapy.srp(broadcast_frame/arp_request, timeout = 1, verbose = False)
     for reply in answered:
-        print(reply[1].psrc, reply[1].hwsrc)
+        print("IP: "reply[1].psrc, "MAC: "reply[1].hwsrc)
 
 
 scan("10.21.100.1")
