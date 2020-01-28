f44e5847e1a38c987a97fa915958365c76e0aaf9
lukianets <lukyanets89@gmail.com>
2020-01-28 16:30:26 +0200

    add Generator Expression

--- python-scapy-arp-scan.py
+++ python-scapy-arp-scan.py
@@ -17,10 +17,10 @@ def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast_frame = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     answered, unanswered = scapy.srp(broadcast_frame / arp_request, timeout=1, verbose=False)
-    list_arp_response = []
-    for reply in answered:
-        list_arp_response.append({"ip": reply[1].psrc, "mac": reply[1].hwsrc})
-    return list_arp_response
+    list_arp_response = [{"ip": reply[1].psrc, "mac": reply[1].hwsrc} for reply in answered]
+  #  for reply in answered:
+  #      list_arp_response.append({"ip": reply[1].psrc, "mac": reply[1].hwsrc})
+  #  return list_arp_response
 
 
 def output_results(list):
